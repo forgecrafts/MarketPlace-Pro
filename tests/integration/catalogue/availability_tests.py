django-oscar/django-oscar

commit 7fd62f2af078f950eeb278b97d01153b90b08e73

[Backport] Fix bug with product class lookups in variants
This causes availability messages to fail in templates as an exception
is raised for variant products which don't define a product class.

Cherry-pick of 72eaafecb356032d198248e485a0fbcdfad5e3f2 back onto
releases branch.

oscar/apps/catalogue/abstract_models.py
@@ -361,7 +361,7 @@ class AbstractProduct(models.Model):
                 if variant.is_available_to_buy:
                     return True
             return False
-        if not self.product_class.track_stock:
+        if self.get_product_class().track_stock:
             return True
         return self.has_stockrecord and self.stockrecord.is_available_to_buy
 

forgecrafts/MarketPlace-Pro

commit 4dcefc0d608e46c6338b847769f225b08785a4a4

Fix logic error in product availability
Introduced in 7fd62f2af078f950eeb278b97d01153b90b08e73.

oscar/apps/catalogue/abstract_models.py
@@ -361,7 +361,7 @@ class AbstractProduct(models.Model):
                 if variant.is_available_to_buy:
                     return True
             return False
-        if self.get_product_class().track_stock:
+        if not self.get_product_class().track_stock:
             return True
         return self.has_stockrecord and self.stockrecord.is_available_to_buy
 

tests/integration/catalogue/availability_tests.py
@@ -1,0 +1,52 @@
+from django import test
+import mock
+
+from oscar.apps.catalogue import models
+from oscar.apps.partner.models import StockRecord
+
+
+class TestAStandaloneProductIsAvailableToBuyWhen(test.TestCase):
+
+    def test_its_product_class_does_not_track_stock(self):
+        product_class = models.ProductClass(
+            track_stock=False)
+        product = models.Product(
+            product_class=product_class)
+        self.assertTrue(product.is_available_to_buy)
+
+    def test_its_stockrecord_indicates_so(self):
+        product_class = models.ProductClass()
+        product = models.Product(
+            product_class=product_class)
+
+        # Create mock version of a model that can be assigned as a FK
+        record = mock.Mock(spec=StockRecord)
+        record._state = mock.Mock()
+        record._state.db = None
+        record.is_available_to_buy = True
+        product.stockrecord = record
+
+        self.assertTrue(product.is_available_to_buy)
+
+
+class TestAStandaloneProductIsNotAvailableToBuyWhen(test.TestCase):
+
+    def test_it_has_no_stock_record(self):
+        product_class = models.ProductClass()
+        product = models.Product(
+            product_class=product_class)
+        self.assertFalse(product.is_available_to_buy)
+
+    def test_its_stockrecord_indicates_so(self):
+        product_class = models.ProductClass()
+        product = models.Product(
+            product_class=product_class)
+
+        # Create mock version of a model that can be assigned as a FK
+        record = mock.Mock(spec=StockRecord)
+        record._state = mock.Mock()
+        record._state.db = None
+        record.is_available_to_buy = False
+        product.stockrecord = record
+
+        self.assertFalse(product.is_available_to_buy)