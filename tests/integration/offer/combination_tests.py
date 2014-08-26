machtfit/django-oscar

Don't create objects in the database unless necessary

tests/integration/offer/combination_tests.py
@@ -10,17 +10,17 @@ from oscar.test import factories
 class TestANoneConditionWithPercentageDiscount(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        condition = models.Condition.objects.create(
+        condition = models.NoneCondition(
             range=range,
             type=models.Condition.NONE)
-        benefit = models.Benefit.objects.create(
+        benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20,
             max_affected_items=1)
-        self.offer = models.ConditionalOffer.objects.create(
+        self.offer = models.ConditionalOffer(
             name="Test",
             offer_type=models.ConditionalOffer.SITE,
             condition=condition,

tests/integration/offer/percentage_benefit_tests.py
@@ -11,12 +11,12 @@ from oscar.test.basket import add_product, add_products
 class TestAPercentageDiscountAppliedWithNoneCondition(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        self.condition = models.NoneCondition.objects.create(
+        self.condition = models.NoneCondition(
             range=range,
             type=models.Condition.NONE)
-        self.benefit = models.PercentageDiscountBenefit.objects.create(
+        self.benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20)
@@ -55,12 +55,12 @@ class TestAPercentageDiscountAppliedWithNoneCondition(TestCase):
 class TestAPercentageDiscountWithMaxItemsSetAppliedWithNoneCondition(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        self.condition = models.NoneCondition.objects.create(
+        self.condition = models.NoneCondition(
             range=range,
             type=models.Condition.NONE)
-        self.benefit = models.PercentageDiscountBenefit.objects.create(
+        self.benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20,

forgecrafts/MarketPlace-Pro

Conflicts:
	oscar/apps/offer/models.py
	tests/integration/offer/combination_tests.py
	tests/integration/offer/percentage_benefit_tests.py
@maikhoepfel duplicated the proxy class short-circuit logic for
Conditions as part of this commit.
Cherry-picked from #1414.

tests/integration/offer/combination_tests.py
@@ -10,18 +10,18 @@ from oscar.test import factories
 class TestACountConditionWithPercentageDiscount(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        condition = models.Condition.objects.create(
+        condition = models.CountCondition(
             range=range,
             type=models.Condition.COUNT,
             value=3)
-        benefit = models.Benefit.objects.create(
+        benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20,
             max_affected_items=1)
-        self.offer = models.ConditionalOffer.objects.create(
+        self.offer = models.ConditionalOffer(
             name="Test",
             offer_type=models.ConditionalOffer.SITE,
             condition=condition,

tests/integration/offer/percentage_benefit_tests.py
@@ -11,13 +11,13 @@ from oscar.test.basket import add_product, add_products
 class TestAPercentageDiscountAppliedWithCountCondition(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        self.condition = models.CountCondition.objects.create(
+        self.condition = models.CountCondition(
             range=range,
             type=models.Condition.COUNT,
             value=2)
-        self.benefit = models.PercentageDiscountBenefit.objects.create(
+        self.benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20)
@@ -56,13 +56,13 @@ class TestAPercentageDiscountAppliedWithCountCondition(TestCase):
 class TestAPercentageDiscountWithMaxItemsSetAppliedWithCountCondition(TestCase):
 
     def setUp(self):
-        range = models.Range.objects.create(
+        range = models.Range(
             name="All products", includes_all_products=True)
-        self.condition = models.CountCondition.objects.create(
+        self.condition = models.CountCondition(
             range=range,
             type=models.Condition.COUNT,
             value=2)
-        self.benefit = models.PercentageDiscountBenefit.objects.create(
+        self.benefit = models.PercentageDiscountBenefit(
             range=range,
             type=models.Benefit.PERCENTAGE,
             value=20,